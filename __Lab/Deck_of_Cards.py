369d711dc146211d1dd966846a9f3d3e06877789
Gregory Freeman <gsfreemanjr@gmail.com>
2025-10-02 19:20:57 -0400

    10-02-2025 7:20pm

--- __Lab/Deck_of_Cards.py
+++ __Lab/Deck_of_Cards.py
@@ -1,13 +1,47 @@
 import random
 
-names = {'Joker':0, 'One':1, 'Two':2, 'Three':3, 'Four':4, 
-         'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 
-         'Jack':10, 'Queen':10, 'King':10, 'Ace':10}
-
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
-
 class Card:
-    def __innit__(self, name, suit, value):
+    def __init__(self, name, suit, value):
         self.name = name
         self.suit = suit
         self.value = value
+    
+    def __str__(self):
+        return f'{self.name} of {self.suit} -> value={self.value}'
+
+
+def main():
+    deck = deck_builder()
+    random.shuffle(deck)
+    for _ in range(2):
+        card = deck.pop(-1)
+        print(f'{card.name} of {card.suit}', end=" | ")  
+    
+    
+
+
+def deck_builder():
+    suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs',]
+    values = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, 
+              '9':9, '10':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
+    deck = []
+
+    for suit in suits:
+        for value in values:
+            card = Card(value, suit, values[value])
+            deck.append(card)
+    return deck
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
