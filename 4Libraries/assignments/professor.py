555995814816f656bd00e1cc036183cb176df35e
Gregory Freeman <gsfreemanjr@gmail.com>
2025-10-02 00:36:39 -0400

    10-02-2025 12:36am

--- 4Libraries/assignments/professor.py
+++ 4Libraries/assignments/professor.py
@@ -1,64 +1,53 @@
 import random
 
-def main():
-    selected_level = get_level()
-    new_problem_set = generate_integer(selected_level)
-    score = 0
 
-    for set in new_problem_set:
-        solution = set[0] + set[1]
-        lives = 2
+def main():
+    selected_level = get_level() # prompts the user to select a level
+    score = 0                                               
+    
+    for _ in range(10):
+        x = generate_integer(selected_level)
+        y = generate_integer(selected_level)
+        
+        solution = x + y
+        lives = 3
         
         while True:
-            print(f'{set[0]} + {set[1]} = ', end="")
             try:
-                user_answer = int(input("").strip())
+                if lives == 0: raise SyntaxError
+                user_answer = int(input(f'{x} + {y} = ').strip())
                 if user_answer != solution: raise ValueError
+                else: score += 1
             except ValueError:
-                if lives == 0:
-                    print('EEE')
-                    print(f'{set[0]} + {set[1]} = {set[0] + set[1]}')
-                    break
-                else:
-                    lives -= 1
-                    print('EEE')
-                pass
+                print('EEE')
+                lives -= 1
+            except SyntaxError:
+                print(f'{x} + {y} = {solution}')
+                break 
             else:
-                score += 1
                 break
 
-    print(f'score: {score}')
- 
+    print(f'Score: {score}')
+
 
 def get_level():
     levels = [1, 2, 3]
+
     while True:
         try:
-            level = int(input('Level: '))
+            level = int(input('Level: ').strip())
             if level not in levels: raise ValueError
-        except ValueError: pass
-        except NameError: pass
-        else: return level
-
-
+        except ValueError:
+            pass
+        else:
+            return level
 def generate_integer(level):
-    problem_set = []
-    for _ in range(10):
-        match level:
-            case 1:
-                int_1 = random.randint(1, 9)
-                int_2 = random.randint(1, 9)
-                problem_set.append((int_1, int_2))
-            case 2:
-                int_1 = random.randint(10, 99)
-                int_2 = random.randint(10, 99)
-                problem_set.append((int_1, int_2))
-            case 3:
-                int_1 = random.randint(100, 999)
-                int_2 = random.randint(100, 999)
-                problem_set.append((int_1, int_2))
-    return problem_set
-    
-
+    match level:
+        case 1:
+            return random.randint(0, 9)
+        case 2:
+            return random.randint(10, 99)
+        case 3:
+            return random.randint(100, 999)
 if __name__ == "__main__":
     main()
